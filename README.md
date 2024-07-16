ad67382695b887783d51168502c834e7352b4637
Joao Pedro Aguiar <139718095+TI0J0A0@users.noreply.github.com>
2024-07-16 19:32:28 -0400

    Setup Inicial do Projeto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+![alt text](image-2.png)
 # Image-Search-Engine
 Creating an Image Search Engine with CSS and JavaScript involves several steps to ensure that the application is functional, user-friendly, and visually appealing.
---
+++ script.js
@@ -0,0 +1,52 @@
+const accessKey = "19rup0Qj1JrAHPFjiNZf_8ON1RgloN7hwUP5TaLiwwo";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages() {
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (page === 1) {
+            searchResult.innerHTML = ""; // Limpar resultados anteriores na nova pesquisa
+        }
+
+        const results = data.results;
+
+        results.forEach((result) => {
+            const image = document.createElement("img");
+            image.src = result.urls.small;
+            const imageLink = document.createElement("a");
+            imageLink.href = result.links.html;
+            imageLink.target = "_blank";
+
+            imageLink.appendChild(image);
+            searchResult.appendChild(imageLink);
+        });
+
+        showMoreBtn.style.display = results.length > 0 ? "block" : "none"; // Mostrar botão "Mostrar mais" se houver resultados
+
+    } catch (error) {
+        console.error("Erro ao buscar imagens:", error);
+    }
+}
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMoreBtn.addEventListener("click", () => {
+    page++;
+    searchImages();
+});
